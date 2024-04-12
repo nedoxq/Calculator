0e44b5761135c4d1238238f2ea4bc4ce9b2d900b
nedoxq <nhodzic098@gmail.com>
2024-04-12 18:41:19 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var display = document.getElementById("display");
+
+function addToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    display.value = eval(display.value);
+}
